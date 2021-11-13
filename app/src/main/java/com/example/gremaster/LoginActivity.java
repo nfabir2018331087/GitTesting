93df3c9a84605e8e5f7cd2586e5b44410dee3e57
Nafis Fuad Abir <nfabir7@gmail.com>
2021-11-13 23:00:33 +0600

    Modified splash, login, signup

--- app/src/main/java/com/example/gremaster/LoginActivity.java
+++ app/src/main/java/com/example/gremaster/LoginActivity.java
@@ -3,7 +3,9 @@ package com.example.gremaster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -23,6 +25,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         textView = (TextView) findViewById(R.id.textView6);
 
         textView.setOnClickListener(this);
+
+        getSupportActionBar().hide();
     }
 
     @Override
