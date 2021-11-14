d2c6793be299c87cb5d38f7d1baa8e7b72eee83c
Nafis Fuad Abir <nfabir7@gmail.com>
2021-11-15 02:22:16 +0600

    connected firebase

--- app/src/main/java/com/example/gremaster/LoginActivity.java
+++ app/src/main/java/com/example/gremaster/LoginActivity.java
@@ -1,30 +1,50 @@
 package com.example.gremaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
 import android.text.Html;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 import org.w3c.dom.Text;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button;
-    TextView textView;
+    private EditText emailEditText, passEditText;
+    private Button logInButton;
+    private TextView textView;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        button = (Button) findViewById(R.id.button);
+        mAuth = FirebaseAuth.getInstance();
+
+        logInButton = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textView6);
+        emailEditText = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        passEditText = (EditText) findViewById(R.id.editTextTextPassword);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         textView.setOnClickListener(this);
+        logInButton.setOnClickListener(this);
 
         getSupportActionBar().hide();
     }
@@ -35,5 +55,49 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LoginActivity.this,SignUpActivity.class);
             startActivity(intent);
         }
+        if(v.getId()==R.id.button) userLogIn();
+    }
+
+    private void userLogIn() {
+
+        String email = emailEditText.getText().toString().trim();
+        String password = passEditText.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            emailEditText.setError("Enter an email address");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Enter a valid email address");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            passEditText.setError("Enter a password");
+            passEditText.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            passEditText.setError("Password length should be 6 or higher");
+            passEditText.requestFocus();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            finish();
+                            Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                        } else Toast.makeText(LoginActivity.this, "LogIn Unsuccessful", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/example/gremaster/SignUpActivity.java
+++ app/src/main/java/com/example/gremaster/SignUpActivity.java
@@ -1,16 +1,105 @@
 package com.example.gremaster;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-public class SignUpActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText emailEditText, passEditText;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private Button signUpButton;
+    private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mAuth = FirebaseAuth.getInstance();
+
         getSupportActionBar().hide();
+
+        emailEditText = (EditText) findViewById(R.id.editTextTextEmailAddress);
+        passEditText = (EditText) findViewById(R.id.editTextTextPassword);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        signUpButton = (Button) findViewById(R.id.button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        signUpButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.button) userRegister();
+    }
+
+    private void userRegister() {
+        String email = emailEditText.getText().toString().trim();
+        String password = passEditText.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            emailEditText.setError("Enter an email address");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailEditText.setError("Enter a valid email address");
+            emailEditText.requestFocus();
+            return;
+        }
+        if (password.isEmpty()) {
+            passEditText.setError("Enter a password");
+            passEditText.requestFocus();
+            return;
+        }
+        if (password.length() < 6) {
+            passEditText.setError("Password length should be 6 or higher");
+            passEditText.requestFocus();
+            return;
+        }
+        if (radioGroup.getCheckedRadioButtonId() == 0) {
+            Toast.makeText(this, "Select any option", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(SignUpActivity.this,"Account created successfully", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(SignUpActivity.this,LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(SignUpActivity.this, "You already have an account", Toast.LENGTH_SHORT).show();
+                                Intent intent1 = new Intent(SignUpActivity.this, LoginActivity.class);
+                                startActivity(intent1);
+                            }
+                            else Toast.makeText(SignUpActivity.this,
+                                    "Error: "+task.getException().getMessage()+" Try Again!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
